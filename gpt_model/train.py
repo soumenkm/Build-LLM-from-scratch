e1838282780c846fa028dead23f7a44f24125ad3
Soumen Mondal <mondalsoumen00@gmail.com>
2024-06-08 02:42:30 +0530

    Add training loop

--- gpt_model/train.py
+++ gpt_model/train.py
@@ -1,7 +1,8 @@
-import torch, torchinfo, json, sys, tiktoken
+import torch, torchinfo, json, sys, tiktoken, pathlib, random
 from gpt import GPTModel
 from dataset import prepare_dataloader, train_val_split
 from typing import List, Tuple
+from tqdm import tqdm
 
 def text_to_token_ids(text: str, tokenizer: "tiktoken.tokenizer") -> torch.tensor:
     
@@ -20,15 +21,17 @@ def token_ids_to_text(token_ids: torch.tensor, tokenizer: "tiktoken.tokenizer") 
     
     return text
     
-def generate_token_ids(start_context: str, model: GPTModel, tokenizer: "tiktoken.tokenizer", max_num_tokens: int=50) -> torch.tensor:
+def generate_text(start_context: str, model: GPTModel, tokenizer: "tiktoken.tokenizer", device: "torch.device", max_num_tokens: int=50) -> str:
 
     inputs = text_to_token_ids(text=start_context, tokenizer=tokenizer) # (1, T)
+    model = model.to(device)
     
     model.eval()
     for i in range(max_num_tokens):
         inputs = inputs[:, :model.Tmax] # (1, T) but at max (1, Tmax)
         
         with torch.no_grad():
+            inputs = inputs.to(device)
             outputs = model(inputs, is_logits=False) # (1, T, V)
         
         max_prob_index = outputs.argmax(dim=-1, keepdim=False) # (1, T)
@@ -38,82 +41,136 @@ def generate_token_ids(start_context: str, model: GPTModel, tokenizer: "tiktoken
         
         inputs = torch.cat([inputs, last_token_pred], dim=-1) # (1, T+1)
     
+    model.train()
     generated_token_ids = inputs # (1, T+Ti)
+    generated_text = token_ids_to_text(token_ids=generated_token_ids, tokenizer=tokenizer)
     
-    return generated_token_ids
+    return generated_text
 
-def calculate_loss_batch(batch: Tuple[torch.tensor, torch.tensor], model: GPTModel, device: "torch.device") -> torch.tensor:
+def evaluate_batch(batch: Tuple[torch.tensor, torch.tensor], model: GPTModel, device: "torch.device", is_train: bool) -> Tuple["torch.tensor", "torch.tensor"]:
     
     inputs, targets = batch # (b, T)
-    inputs, targets = inputs.to(device), targets.to(device)
+    inputs, targets = inputs.to(device), targets.to(device) # (b, T)
     model = model.to(device)
     
-    pred_logits = model(inputs) # (b, T, V)
-    pred_logits = pred_logits.flatten(start_dim=0, end_dim=1)
-    targets = targets.flatten(start_dim=0, end_dim=1)
+    if is_train:
+        model.train()
+        pred_logits = model(inputs) # (b, T, V)
+    else:
+        model.eval()
+        with torch.no_grad():
+            pred_logits = model(inputs) # (b, T, V)
+        model.train()
+        
+    pred_logits = pred_logits.flatten(start_dim=0, end_dim=1) # (b*T, V)
+    targets = targets.flatten(start_dim=0, end_dim=1) # (b*T,)
     
-    loss = torch.nn.functional.cross_entropy(input=pred_logits, target=targets)
-    
-    return loss
+    loss_metric = torch.nn.functional.cross_entropy(input=pred_logits, target=targets)
+    acc_metric = (pred_logits.argmax(dim=-1) == targets).to(torch.float32).mean()
 
-def calculate_loss_dataloader(dataloader: "torch.dataloader", model: GPTModel, device: "torch.device", batch_fraction: float=0.2) -> float:
+    return loss_metric, acc_metric
+
+def evaluate_dataloader(dataloader: "torch.dataloader", model: GPTModel, device: "torch.device", is_train: bool, batch_fraction: float) -> float:
     
     num_batches = int(len(dataloader) * batch_fraction) # a small num of batches for speed
+    random_batches = random.sample(list(dataloader), num_batches)
     
-    loss_list = []
-    for i, batch in enumerate(dataloader):
-        
-        loss_batch = calculate_loss_batch(batch=batch, model=model, device=device)
-        loss_list.append(loss_batch.item())
-        
-        if i >= num_batches:
-            break
+    loss_metric_list = []
+    acc_metric_list = []
     
-    loss_dl = sum(loss_list)/len(loss_list)
+    for batch in random_batches:
+        loss_metric_batch, acc_metric_batch = evaluate_batch(batch=batch, model=model, device=device, is_train=is_train)
+        loss_metric_list.append(loss_metric_batch.item())
+        acc_metric_list.append(acc_metric_batch.item())
     
-    return loss_dl
-        
-# Set the device
-if torch.cuda.is_available():
-    device_type = "cuda"
-    print("Using GPU...")
-    print(f"Total # of GPU: {torch.cuda.device_count()}")
-    print(f"GPU Details: {torch.cuda.get_device_properties(device=torch.device(device_type))}")
-else:
-    device_type = "cpu"
-    print("Using CPU...")
+    loss_metric_dl = sum(loss_metric_list)/len(loss_metric_list)
+    acc_metric_dl = sum(acc_metric_list)/len(acc_metric_list)
+    
+    return loss_metric_dl, acc_metric_dl
 
-device = torch.device(device_type)
+def train(num_epochs: int, model: GPTModel, optimizer: "torch.optimizer", train_dl: "torch.dataloader", val_dl: "torch.dataloader", device: "torch.device") -> dict:
+    
+    model = model.to(device)
 
-BATCH_SIZE = 2
-CONFIG = {
+    train_loss_ep = []
+    train_acc_ep = []
+    val_loss_ep = []
+    val_acc_ep = []
+    
+    for ep in range(num_epochs):
+        train_loss_list = []
+        train_acc_list = []
+        num_steps = len(train_dl)
+        
+        with tqdm(train_dl, desc=f"Epoch: {ep+1}/{num_epochs}", postfix={"train_batch_loss": 0, "train_batch_acc": 0}, colour="green") as pbar:
+        
+            for i, train_batch in enumerate(pbar):           
+                optimizer.zero_grad() # resets the gradients to zero for this batch
+                loss, acc = evaluate_batch(batch=train_batch, model=model, device=device, is_train=True)
+                loss.backward() # calculates the gradients of the loss w.r.t. model parameters
+                optimizer.step() # performs gradient descent step on model paramters
+                
+                train_loss_list.append(loss.item())
+                train_acc_list.append(acc.item())
+            
+                pbar.set_postfix({"train_batch_loss": f"{loss:.3f}", "train_batch_acc": f"{acc:.3f}"})
+            
+        train_loss = sum(train_loss_list)/len(train_loss_list)
+        train_acc = sum(train_acc_list)/len(train_acc_list)
+            
+        val_loss, val_acc = evaluate_dataloader(dataloader=val_dl, model=model, device=device, is_train=False, batch_fraction=0.25)
+
+        train_loss_ep.append(train_loss)
+        train_acc_ep.append(train_acc)
+        val_loss_ep.append(val_loss)
+        val_acc_ep.append(val_acc)
+        
+        print(f"Epoch: {ep+1}/{num_epochs}, train_epoch_loss: {train_loss:.3f}, train_epoch_acc: {train_acc:.3f}, val_epoch_loss: {val_loss:.3f}, val_epoch_acc: {val_acc:.3f}")
+    
+    history = {"train": {"loss": train_loss_ep, "acc": train_acc_ep},
+            "val": {"loss": val_loss_ep, "acc": val_acc_ep}}
+
+    return history
+    
+if __name__ == "__main__":
+    
+    if torch.cuda.is_available():
+        device_type = "cuda"
+        print("Using GPU...")
+        print(f"Total # of GPU: {torch.cuda.device_count()}")
+        print(f"GPU Details: {torch.cuda.get_device_properties(device=torch.device(device_type))}")
+    else:
+        device_type = "cpu"
+        print("Using CPU...")
+
+    device = torch.device(device_type)
+
+    BATCH_SIZE = 2
+    CONFIG = {
         "vocab_size": 50257,
         "max_context_length": 256,
         "emb_dim": 768,
         "n_heads": 12,
         "n_layers": 12,
         "drop_rate": 0.1
-}
-tokenizer = tiktoken.get_encoding("gpt2")
-model = GPTModel(config=CONFIG)
+    }
+    IS_TRAIN = True
+    tokenizer = tiktoken.get_encoding("gpt2")
+    model = GPTModel(config=CONFIG)
 
-file_list = ["/Users/soumen/Desktop/IITB/sem2/LLM/Build-LLM-from-scratch/tokenizer/corpus/the-verdict.txt"]*2
-train_text, val_text = train_val_split(file_list=file_list,
-                                       train_val_split_ratio=0.8)
+    corpus_path = pathlib.Path("/Users/soumen/Desktop/IITB/sem2/LLM/Build-LLM-from-scratch/tokenizer/corpus")
+    file_list = [str(i) for i in corpus_path.iterdir()]
 
-train_dl = prepare_dataloader(text=train_text, 
-                              tokenizer=tokenizer,
-                              max_context_length=CONFIG["max_context_length"],
-                              batch_size=BATCH_SIZE,
-                              is_train=True)
+    train_text, val_text = train_val_split(file_list=file_list, train_val_split_ratio=0.8)
+    train_dl = prepare_dataloader(text=train_text, tokenizer=tokenizer, max_context_length=CONFIG["max_context_length"], batch_size=BATCH_SIZE, is_train=True)
+    val_dl = prepare_dataloader(text=val_text, tokenizer=tokenizer, max_context_length=CONFIG["max_context_length"], batch_size=BATCH_SIZE, is_train=False)
 
-val_dl = prepare_dataloader(text=val_text, 
-                            tokenizer=tokenizer,
-                            max_context_length=CONFIG["max_context_length"],
-                            batch_size=BATCH_SIZE,
-                            is_train=False)
-
-loss = calculate_loss_batch(batch=train_dl.__iter__().__next__(), model=model, device=device)
-print(loss)
-loss1 = calculate_loss_dataloader(dataloader=val_dl, model=model, device=device)
-print(loss1)
+    optimizer = torch.optim.AdamW(params=model.parameters(), lr=0.0004, betas=(0.9, 0.999), weight_decay=0.1)
+    
+    if IS_TRAIN:
+        history = train(num_epochs=1, model=model, optimizer=optimizer, train_dl=train_dl, val_dl=val_dl, device=device)
+        torch.save(model.state_dict(), pathlib.Path(pathlib.Path.cwd(), "model.pth"))
+    else:
+        model.load_state_dict(torch.load(pathlib.Path(pathlib.Path.cwd(), "model.pth")))
+        text = generate_text(start_context="Hello world!", model=model, tokenizer=tokenizer, device=device, max_num_tokens=20)
+        print(text)
